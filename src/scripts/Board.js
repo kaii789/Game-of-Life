ec01446e4c6c32fe34b3c2bb87a70fc3b957131e
Kailong <kailong9@gmail.com>
2018-12-27 22:32:23 -0500

    trying to make project modular

---
+++ src/scripts/Board.js
@@ -0,0 +1,32 @@
+export class Board {
+    constructor(tileSizePx) {
+        this.tileSize = tileSizePx;
+        this.tiles = _generateTiles();
+    }
+
+    _generateTiles() {
+        // based off tileSize, generate the appropriate number of tiles
+        var rows = Math.floor(window.innerHeight / this.tileSize);
+        var columns = Math.floor(window.innerHeight / this.tileSize);
+        var tiles = new Array();
+        for (var i = 0; i < rows; i++) {
+            for (var j = 0; j < columns; j++) {
+                tiles[i][j] = 0;
+            }
+        }
+
+        return tiles;
+    }
+
+    updateGeneration() {
+        // iterate through tiles
+            // for a given tile, if alive, check num of alive neighbors >> if not 2 or 3 alive neighbors, then dead in next gen
+            // if dead, if 3 alive neighbors, alive in next gen
+        
+        // for (var i = 0)
+    }
+
+    draw() {
+
+    }
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,5 @@
+import { Board } from "Board";
+
 // assignments
 var canvas = document.querySelector("canvas");
 var brush = canvas.getContext("2d");
@@ -13,37 +15,3 @@ function animate() {
     board.draw();
 }
 
-
-//________________________________________________________
-class Board {
-    constructor(tileSizePx) {
-        this.tileSize = tileSizePx;
-        this.tiles = _generateTiles();
-    }
-
-    _generateTiles() {
-        // based off tileSize, generate the appropriate number of tiles
-        var rows = Math.floor(window.innerHeight / this.tileSize);
-        var columns = Math.floor(window.innerHeight / this.tileSize);
-        var tiles = new Array();
-        for (var i = 0; i < rows; i++) {
-            for (var j = 0; j < columns; j++) {
-                tiles[i][j] = 0;
-            }
-        }
-
-        return tiles;
-    }
-
-    updateGeneration() {
-        // iterate through tiles
-            // for a given tile, if alive, check num of alive neighbors >> if not 2 or 3 alive neighbors, then dead in next gen
-            // if dead, if 3 alive neighbors, alive in next gen
-        
-        // for (var i = 0)
-    }
-
-    draw() {
-
-    }
-}
